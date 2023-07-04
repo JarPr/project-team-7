02923f358646a479082d4df127ca686296d57303
StarovDrii <131916457+StarovDrii@users.noreply.github.com>
2023-07-05 00:01:07 +0300

    modify

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,6 +5,8 @@
     burger: document.querySelector("[data-burger]"),
     menu: document.querySelector("[data-menu]"),
     closeMenuLink: document.querySelector("[data-list-close]"),
+    closeMenuLink1: document.querySelector("[data-list-close1]"),
+    closeMenuLink2: document.querySelector("[data-list-close2]"),
     
    
   };
@@ -12,11 +14,15 @@
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuLink.addEventListener("click", toggleMenu);
+  refs.closeMenuLink1.addEventListener("click", toggleMenu);
+  refs.closeMenuLink2.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
     refs.burger.classList.toggle("is-hidden");
     refs.link.classList.toggle("is-hidden");
+    refs.link1.classList.toggle("is-hidden");
+    refs.link2.classList.toggle("is-hidden");
     document.body.classList.toggle('no-scroll')
   }
 })();
