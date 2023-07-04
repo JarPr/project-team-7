50605fba5a37f1ef8796b92c32ca02f13cab649f
StarovDrii <131916457+StarovDrii@users.noreply.github.com>
2023-07-04 23:51:02 +0300

    Fix menu
    Вирішення проблеми при переході якірними посиланнями

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,16 +2,21 @@
   const refs = {
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menu: document.querySelector("[data-menu]"),
     burger: document.querySelector("[data-burger]"),
+    menu: document.querySelector("[data-menu]"),
+    closeMenuLink: document.querySelector("[data-list-close]"),
+    
+   
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuLink.addEventListener("click", toggleMenu);
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
     refs.burger.classList.toggle("is-hidden");
-      document.body.classList.toggle('no-scroll')
+    refs.link.classList.toggle("is-hidden");
+    document.body.classList.toggle('no-scroll')
   }
 })();
